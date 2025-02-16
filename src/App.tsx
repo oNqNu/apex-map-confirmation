694c2ca5cbdd56c49a78bb1df02ea49ad67af3a2
oNqNu <onanukintaro@gmail.com>
2025-02-16 20:06:41 +0900

    refactor: update TypeScript configuration and ESLint setup for improved linting and module detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    
-      <h1 className="hello-world">hello world</h1>
-    
+
+    <h1 className="hello-world">hello world</h1>
+
   )
 }
 
