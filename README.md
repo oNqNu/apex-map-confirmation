138525288bba7177afce32bf898165296731cbb8
oNqNu <onanukintaro@gmail.com>
2025-02-16 19:21:50 +0900

    initialize React + TypeScript + Vite template with basic configuration and hello world example

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# apex-map-confirmation
+# React + TypeScript + Vite
+
+This template provides a minimal setup to get React working in Vite with HMR and displays "hello world" on the screen.
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    
+      <h1 style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        margin: 0,
+      }}>hello world</h1>
+    
+  )
+}
+
+export default App
